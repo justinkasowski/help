f823791f0f4e1d88ca56be42236fc919a762f529
JustinKasowski <justin_kasowski@ucsb.edu>
2021-04-05 14:36:13 -0700

    Initial commit

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string manu = "Lamborghini";
+    const char *const c_manu = manu.c_str();
+
+    string new_manu = new string(manu);
+    manu[0] = 'P';
+
+    cout << c_manu << endl;
+    cout << new_manu << endl;
+
+}
+
+
+
+
